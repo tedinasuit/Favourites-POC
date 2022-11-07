38a0e12e7235fcf0a738526558fc2abdf561c688
tedinasuit <larshoeijmans@gmail.com>
2022-11-07 08:04:29 +0100

    Fourth Iteration
    This test is coming closer to the final product. Pressing the favourite button, stores Brazil to the localstorage. The javascript code then creates a new div for the 'favourites' list, with the same innerHTML. Essenrially copying the same div over to another place and storing this in LocalStorage.

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-const colorBoxes = document.getElementsByClassName('color-boxes')[0];
-const color = document.getElementById('color');
-const hex = document.getElementById('hex');
-let colorsArray = localStorage.getItem('items') ?
+const ul = document.querySelector(".myclass");
+const input = document.getElementById('item');
+const brazil = document.getElementById('Brazil');
+const greece = document.getElementById('Greece');
+let itemsArray = localStorage.getItem('items') ?
 JSON.parse(localStorage.getItem('items')) : [];
 
-colorsArray.forEach(divMaker);
-function divMaker(text){
-    const div = document.createElement('div');
-    div.className = 'box';
-    div.style.background = text.color_name;
-    div.innerHTML = '<h2>'+text.color_name+'</h2>' +
-                    '<h2>'+text.hex_code+'</h2>';
-    colorBoxes.appendChild(div);
+itemsArray.forEach(divMaker);
+function divMaker (text){
+    const div = document.createElement('div')
+    div.innerHTML = text;
+    ul.appendChild(div);
 }
 
-function add(){
-    var color_info = {
-        'color_name' : color.value,
-        'hex_code' : hex.value
-    }
-    colorsArray.push(color_info);
-    localStorage.setItem('items', JSON.stringify(colorsArray));
-    divMaker(colorsArray[colorsArray.length -1]);
-    color.value = '';
-    hex.value = '';
+function addB(){
+    itemsArray.push(brazil.innerHTML);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    divMaker(brazil.innerHTML);
+}
+
+function delB(){
+    localStorage.clear();
+    ul.innerHTML = '';
+    itemsArray = [];
 }
